506366a52adea6a4f17b03655a5ef131b713bbc1
Alex Baranowski <alex@euro-linux.com>
2020-01-19 16:34:40 +0100

    els-ASAP auto commit file/tag vino-3.14.2-1.el7.src.rpm

---
+++ src/common/vino-settings-enums.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2003 Sun Microsystems, Inc.
+ * Copyright (C) 2006 Jonh Wendell <wendell@bani.com.br> 
+ * Copyright © 2010 Codethink Limited
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Authors:
+ *      Mark McLoughlin <mark@skynet.ie>
+ *      Jonh Wendell <wendell@bani.com.br>
+ *      Ryan Lortie <desrt@desrt.ca>
+ */
+
+typedef enum
+{
+  VINO_ICON_VISIBILITY_NEVER,
+  VINO_ICON_VISIBILITY_ALWAYS,
+  VINO_ICON_VISIBILITY_CLIENT
+} VinoIconVisibility;
